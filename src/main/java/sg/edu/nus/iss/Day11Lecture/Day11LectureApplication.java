5a588d2e69f9ff24afb7b0c7e4280881555ac5d5
Khairul <Khairulimran6810@gmail.com>
2023-11-21 10:06:03 +0800

    Testing

---
+++ src/main/java/sg/edu/nus/iss/Day11Lecture/Day11LectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.Day11Lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day11LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day11LectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/Day11Lecture/Day11LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.Day11Lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day11LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
